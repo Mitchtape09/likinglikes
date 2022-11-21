830b31a0500103700e3bb64d310e9530d8463336
Mitchtape09 <111250040+Mitchtape09@users.noreply.github.com>
2022-11-21 09:58:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function addLikes1()
+{
+    document.querySelector(".likes1").innerHTML++;
+    // if(countElement = document.querySelector(".likes1"))
+    // {
+    //     count++;
+    //     countElement.innerHTML = count + " like(s)";
+    // }
+}
+
+function addLikes2()
+{
+    document.querySelector(".likes2").innerHTML++
+    // if(countElement = document.querySelector(".likes2"))
+    // {
+    //     count++;
+    //     countElement.innerHTML = count + " like(s)";
+    // }
+}
+
+function addLikes3()
+{
+    document.querySelector(".likes3").innerHTML++
+    // if(countElement = document.querySelector(".likes3"))
+    // {
+    //     count++;
+    //     countElement.innerHTML = count + " like(s)";
+    // }
+}
